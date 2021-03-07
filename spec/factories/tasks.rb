00264aed889de25ce41e4d35fd06a3aae8d335fa
Shogo <sho5tkmr0802@gmail.com>
2021-03-07 22:44:08 +0900

    Fix: factorybot_tasks_users

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :task do
-    title { 'testtitle' }
-    content { 'testcontent' }
+    # sequenceで重複しないデータを作成
+    sequence(:title, "title_1")
+    content { "content" }
     status { :todo }
+    deadline { 1.week.from_now }
+    association :user
   end
 end
