00e1c9c763bb57624288c9d8979d2d9b15e09f61
Shogo <sho5tkmr0802@gmail.com>
2021-03-14 15:16:56 +0900

    Fix user_sessions

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe "UserSessions", type: :system do
     context 'フォームの入力値が正常' do
       it 'ログイン処理が成功する' do
         visit login_path
-        fill_in 'email', with: user.email
-        fill_in 'password', with: 'password'
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: 'password'
         click_button 'Login'
         expect(page).to have_content 'Login successful'
         expect(current_path).to eq root_path
@@ -17,8 +17,8 @@ RSpec.describe "UserSessions", type: :system do
     context 'フォームが未入力' do
       it 'ログイン処理が失敗する' do
         visit login_path
-        fill_in 'email', with: ''
-        fill_in 'password', with: ''
+        fill_in 'Email', with: ''
+        fill_in 'Password', with: 'password'
         click_button 'Login'
         expect(page).to have_content 'Login failed'
         expect(current_path).to eq login_path
